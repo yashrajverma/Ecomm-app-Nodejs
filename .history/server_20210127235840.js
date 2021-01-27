bfec102d6b1c8f6e0f9688e7ba4df5188cf67a2f
yashrajverma <yashrajverma798@gmail.com>
2021-01-28 00:00:08 +0530

    Express Setup Done!

---
+++ .history/server_20210127235840.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express()
+const _PORT = 3330;
+
+app.get('/', (req, res) => {
+    res.send("Hello there!");
+})
+
+app.listen(_PORT,
+    console.log("Server is running on PORT:" + _PORT)
+);
