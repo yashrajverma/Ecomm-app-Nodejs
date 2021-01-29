c55cfc93ed9982c0980552f6dbadded5090ee887
yashrajverma <yashrajverma798@gmail.com>
2021-01-29 13:27:03 +0530

    Engine embedded

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express()
 const _PORT = 3330;
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
 app.get('/', (req, res) => {
-    res.send("Hello there!");
-})
+    res.render('index')
+});
 
 app.listen(_PORT,
     console.log("Server is running on PORT:" + _PORT)
